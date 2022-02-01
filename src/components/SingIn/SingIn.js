b3f8c8defb8f0ad122718a0ca37fcd6cbb25a034
Vladislav Tonkohog <48497062+VlTonko@users.noreply.github.com>
2022-02-02 00:53:49 +0200

    fixed signIn/signUp

--- src/components/SingIn/SingIn.js
+++ src/components/SingIn/SingIn.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {withRouter} from "react-router";
+import { withRouter } from 'react-router';
 import axios from 'axios';
 import Form from '../common/Input/Form';
 import Input from '../common/Input/InputField';
-import { URL_SIGN_IN} from '../../constants/api';
+import { URL_SIGN_IN } from '../../constants/api';
 import Button from '../common/Button/Button';
 import '../SingIn/SingIn.scss';
-import {isValidLogin, isValidPassword} from '../../helpers/validation';
+import { isValidLogin, isValidPassword } from '../../helpers/validation';
 
 class SingIn extends Component {
     constructor(props) {
@@ -25,68 +25,51 @@ class SingIn extends Component {
 
     handleChangePassword = value => this.setState({ password: value });
 
-    handleChangePasswordInput = () =>{
+    handleChangePasswordInput = () => {
         const error = isValidPassword(this.state.password);
-        this.setState({error: {...this.state.error,password: error}});
-    }
-    handleChangeLoginInput = () =>{
+        this.setState({ error: { ...this.state.error, password: error } });
+    };
+    handleChangeLoginInput = () => {
         const error = isValidLogin(this.state.user);
-        this.setState({error: {...this.state.error,user: error}});
-    }
+        this.setState({ error: { ...this.state.error, user: error } });
+    };
 
-    handleSubmitFormSignIn = async (event) => {
+    handleSubmitFormSignIn = async event => {
         event.preventDefault();
         try {
-            // helpers.setButtonLoader(domElements.signInButton);
-            //возможно валидация
+            this.setState({ isLoading: true });
             const body = { login: this.state.user, password: this.state.password };
-            const {headers} = await axios.post(URL_SIGN_IN, body);
+            const { headers } = await axios.post(URL_SIGN_IN, body);
             localStorage.setItem('token', headers.token);
             this.props.history.push('/movies');
         } finally {
-            // helpers.removeButtonLoader(domElements.signInButton);
+            this.setState({ isLoading: false });
         }
     };
 
     render() {
         return (
-            <div className="container main__container">
-                <div className="form-wrapper">
-                    <Form onSubmit={this.handleSubmitFormSignIn}>
-                        <div className="fields">
-                            <div className="input-wrapper">
-                                <Input 
-                                    label={'Login'} 
-                                    type={'text'} 
-                                    name={'login'} 
-                                    value={this.state.user} 
-                                    placeholder={'Enter your login'} 
-                                    autocomplete="off" 
-                                    onChange={this.handleChangeUser}
-                                    onBlur={this.handleChangeLoginInput}
-                                    error={this.state.error.user}
-                                />
-                                <Input
-                                    label={'Password'}
-                                    type={'password'}
-                                    name={'password'}
-                                    value={this.state.password}
-                                    placeholder={'Enter your password'}
-                                    autocomplete="off"
-                                    onChange={this.handleChangePassword}
-                                    onBlur={this.handleChangePasswordInput}
-                                    error={this.state.error.password}
-                                />
-                                <Button 
-                                    type={'button'} 
-                                    isDisabled={false} 
-                                    className={''} 
-                                    onClick={this.handleSubmitFormSignIn} 
-                                    contentKey={'SUBMIT'} />
-                            </div>
-                        </div>
-                    </Form>
-                </div>
+            <div className="form-wrapper">
+                <Form onSubmit={this.handleSubmitFormSignIn}>
+                    <Input
+                        label={'Login'}
+                        value={this.state.user}
+                        placeholder={'Enter your login'}
+                        onChange={this.handleChangeUser}
+                        onBlur={this.handleChangeLoginInput}
+                        error={this.state.error.user}
+                    />
+                    <Input
+                        label={'Password'}
+                        type={'password'}
+                        value={this.state.password}
+                        placeholder={'Enter your password'}
+                        onChange={this.handleChangePassword}
+                        onBlur={this.handleChangePasswordInput}
+                        error={this.state.error.password}
+                    />
+                    <Button type="submit" contentKey={'SUBMIT'} isLoading={this.state.isLoading} />
+                </Form>
             </div>
         );
     }
--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {withRouter} from "react-router";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import axios from 'axios';
 import Form from '../common/Input/Form';
 import Input from '../common/Input/InputField';
 import Button from '../common/Button/Button';
-import {URL_SIGN_UP} from '../../constants/api';
-import {isValidLogin, isValidName, isValidPassword} from '../../helpers/validation';
-import {normalizeFilters} from '../../helpers/format';
+import { URL_SIGN_UP } from '../../constants/api';
+import { isValidLogin, isValidName, isValidPassword } from '../../helpers/validation';
+import { normalizeFilters } from '../../helpers/format';
 
 class SingUp extends Component {
     constructor(props) {
@@ -16,7 +16,7 @@ class SingUp extends Component {
             lastName: '',
             userName: '',
             password: '',
-            isLoading:false,
+            isLoading: false,
             error: {
                 firstName: '',
                 lastName: '',
@@ -26,99 +26,92 @@ class SingUp extends Component {
         };
     }
 
-    handleChangeFirstName = value => this.setState({firstName: value});
+    handleChangeFirstName = value => this.setState({ firstName: value });
 
-    handleChangeLastName = value => this.setState({lastName: value});
+    handleChangeLastName = value => this.setState({ lastName: value });
 
-    handleChangeLogin = value => this.setState({userName: value});
+    handleChangeLogin = value => this.setState({ userName: value });
 
-    handleChangePassword = value => this.setState({password: value});
+    handleChangePassword = value => this.setState({ password: value });
 
     // setFilter = (key,value) => this.setState({filters : {...this.state.filters,[key]: value}});
 
-    handleChangeFirstNameInput = () =>{
+    handleChangeFirstNameInput = () => {
         const error = isValidName(this.state.firstName);
-        this.setState({error: {...this.state.error,firstName: error}});
-    }
-    handleChangeLastNameInput = () =>{
+        this.setState({ error: { ...this.state.error, firstName: error } });
+    };
+    handleChangeLastNameInput = () => {
         const error = isValidName(this.state.lastName);
-        this.setState({error: {...this.state.error,lastName: error}});
-    }
-    handleChangePasswordInput = () =>{
+        this.setState({ error: { ...this.state.error, lastName: error } });
+    };
+    handleChangePasswordInput = () => {
         const error = isValidPassword(this.state.password);
-        this.setState({error: {...this.state.error, password: error}});
-    }
-    handleChangeLoginInput = () =>{
+        this.setState({ error: { ...this.state.error, password: error } });
+    };
+    handleChangeLoginInput = () => {
         const error = isValidLogin(this.state.userName);
-        this.setState({error: {...this.state.error,userName: error}});
-    }
+        this.setState({ error: { ...this.state.error, userName: error } });
+    };
 
-
-    handleSubmitFormSignUp = async (event) => {
+    handleSubmitFormSignUp = async event => {
         event.preventDefault();
-        if(Object.keys(normalizeFilters(this.state.error)).length || !this.state.password) return;
+        if (Object.keys(normalizeFilters(this.state.error)).length || !this.state.password) return;
         try {
-            this.setState({isLoading:true});
+            this.setState({ isLoading: true });
             const body = {
                 first_name: this.state.firstName,
                 last_name: this.state.lastName,
                 login: this.state.userName,
-                password: this.state.password
+                password: this.state.password,
             };
             const response = await axios.post(URL_SIGN_UP, body);
-            const {headers} = response;
+            const { headers } = response;
             this.props.history.push('/');
-            return {status: headers.status};
+            return { status: headers.status };
         } finally {
-            this.setState({isLoading:false});
+            this.setState({ isLoading: false });
         }
-    }
+    };
 
     render() {
         return (
-            <div className="container main__container">
-                <div className="form-wrapper">
-                    <Form onSubmit={this.handleSubmitFormSignUp}>
-                        <div className="fields">
-                            <div className="input-wrapper">
-                                <Input
-                                    label={'First Name'}
-                                    value={this.state.firstName}
-                                    placeholder={'Enter your login'}
-                                    onChange={this.handleChangeFirstName}
-                                    onBlur={this.handleChangeFirstNameInput}
-                                    error={this.state.error.firstName}
-                                />
-                                <Input
-                                    label={'Last Name'}
-                                    value={this.state.lastName}
-                                    placeholder={'Enter your password'}
-                                    onChange={this.handleChangeLastName}
-                                    onBlur={this.handleChangeLastNameInput}
-                                    error={this.state.error.lastName}
-                                />
-                                <Input
-                                    label={'Login'}
-                                    value={this.state.userName}
-                                    placeholder={'Enter your password'}
-                                    onChange={this.handleChangeLogin}
-                                    onBlur={this.handleChangeLoginInput}
-                                    error={this.state.error.userName}
-                                />
-                                <Input
-                                    label={'Password'}
-                                    type={'password'}
-                                    value={this.state.password}
-                                    placeholder={'Enter your password'}
-                                    onChange={this.handleChangePassword}
-                                    onBlur={this.handleChangePasswordInput}
-                                    error={this.state.error.password}
-                                />
-                                <Button type="submit" contentKey={'SUBMIT'} isLoading={this.state.isLoading}/>
-                            </div>
-                        </div>
-                    </Form>
-                </div>
+            <div className="form-wrapper">
+                <Form onSubmit={this.handleSubmitFormSignUp}>
+                    <Input
+                        label={'First Name'}
+                        value={this.state.firstName}
+                        placeholder={'Enter your first name'}
+                        onChange={this.handleChangeFirstName}
+                        onBlur={this.handleChangeFirstNameInput}
+                        error={this.state.error.firstName}
+                    />
+                    <Input
+                        label={'Last Name'}
+                        value={this.state.lastName}
+                        placeholder={'Enter your last name'}
+                        onChange={this.handleChangeLastName}
+                        onBlur={this.handleChangeLastNameInput}
+                        error={this.state.error.lastName}
+                    />
+                    <Input
+                        label={'Login'}
+                        value={this.state.userName}
+                        placeholder={'Enter your login'}
+                        onChange={this.handleChangeLogin}
+                        onBlur={this.handleChangeLoginInput}
+                        error={this.state.error.userName}
+                    />
+                    <Input
+                        label={'Password'}
+                        type={'password'}
+                        value={this.state.password}
+                        placeholder={'Enter your password'}
+                        onChange={this.handleChangePassword}
+                        onBlur={this.handleChangePasswordInput}
+                        error={this.state.error.password}
+                    />
+                    <Button type="submit" contentKey={'SUBMIT'} isLoading={this.state.isLoading} />
+                </Form>
             </div>
         );
     }
