4eafa8e65c850a9cfadf9ae80c08983529f41390
NataliaHuberna <natalyahuberna@gmail.com>
2022-02-07 19:11:12 +0200

    added mock state for movies

--- src/components/Movies/__test__/Movies.test.js
+++ src/components/Movies/__test__/Movies.test.js
@@ -1,142 +1,46 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import Movies from '../Movies';
-import axios from 'axios';
-import mockMovies from './__movies-mock__';
-import { act } from 'react-dom/test-utils';
-import { BrowserRouter } from 'react-router-dom';
-import { FILTERS_INIT } from '../../../constants/filters';
-
-const newFilterKey = 'budget_min';
-const newFilterValue = 21000000;
-
-function Filters({ filters }) {
-    return Object.keys(filters).map((item) => <p key={item}>{item}</p>);
-}
-
-function MockedFilters({ filters, setFilter, clearFilters, closeModal, onSubmite }) {
-    return (
-        <>
-            <Filters filters={filters} />
-            <button data-testid="submit-reset-filters" onClick={onSubmite}>
-                Submit reset filters
-            </button>
-            <button data-testid="close-modal" onClick={closeModal}>
-                Set new Filters
-            </button>
-            <button data-testid="set-filters" onClick={() => setFilter(newFilterKey, newFilterValue)}>
-                Set new Filters
-            </button>
-            <button data-testid="clear-filters" onClick={clearFilters}>
-                Clear filters
-            </button>
-        </>
-    );
-}
-
-jest.mock('../Filters/Filters.js', () => {
-    return MockedFilters;
-});
-jest.mock('axios');
+import {FILTERS_INIT} from "../../../constants/filters";
+// import axios from 'axios';
+// import mockMovies from './__movies-mock__';
+// import { act } from 'react-dom/test-utils';
+// import { BrowserRouter } from 'react-router-dom';
+// import { FILTERS_INIT } from '../../../constants/filters';
+// import {COLORS} from "../../common/styles/colors";
 
 describe('Movies', () => {
-    beforeEach(() => {
-        window.scrollTo = jest.fn();
-        axios.get.mockReturnValue(Promise.resolve({ data: mockMovies }));
-    });
-    it('should render all movies after success get request', async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        component.update();
-        expect(component.find('MovieCard').length).toBe(mockMovies.length);
+
+    it('should render correctly', () => {
+        const component = shallow(<Movies/>);
+        expect(component).toMatchSnapshot();
     });
 
-    it(`should set filters to passed key=${newFilterKey} value=${newFilterValue}`, async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        component.find("[data-testid='set-filters']").simulate('click');
-        expect(component.find('Filters').props().filters[newFilterKey]).toBe(newFilterValue);
+    it ('should render prop filmTitle', () => {
+        // console.log(component.debug());
+        const setStateMock = jest.fn();
+        React.useState = jest.fn().mockReturnValue([{
+            movies: [],
+            filters: FILTERS_INIT,
+            currentPage: 1,
+            isFiltersHidden: true,
+            isLoading: true,
+        }, setStateMock]);
+        const component = mount(<Movies/>);
+        console.log(component.debug());
+        component.find('InputField').at(0).getElement().props.onChange({target:{ value : 'bla bla'}});
+        //console.log(component.find('button').at(0).getElement().props);
+        expect(setStateMock).toHaveBeenCalled();
+        //expect(component.find('span').at(0).text()).toEqual(props.title);
     });
 
-    it('should reset filters to initial state', async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        component.find("[data-testid='set-filters']").simulate('click');
-        expect(component.find('Filters').props().filters[newFilterKey]).toBe(newFilterValue);
-        component.find("[data-testid='clear-filters']").simulate('click');
-        expect(component.find('Filters').props().filters[newFilterKey]).toBe(FILTERS_INIT[newFilterKey]);
-    });
-
-    it('should get new filtered movies', async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        let filteredMovies = mockMovies.filter((item) => item.budget <= newFilterValue);
-        await act(async () => {
-            axios.get.mockReturnValue(Promise.resolve({ data: filteredMovies }));
-            await component.find("[data-testid='set-filters']").simulate('click');
-            await component.find("[data-testid='submit-reset-filters']").simulate('click');
-        });
-        component.update();
-        expect(component.find('MovieCard').length).toBe(filteredMovies.length);
-    });
-
-    it('should render NoData Component if movies are empty', async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        let filteredMovies = mockMovies.filter((item) => item.budget > 999999999999999);
-        await act(async () => {
-            axios.get.mockReturnValue(Promise.resolve({ data: filteredMovies }));
-            await component.find("[data-testid='set-filters']").simulate('click');
-            await component.find("[data-testid='submit-reset-filters']").simulate('click');
-        });
-        component.update();
-        expect(component.find('MovieCard').exists()).toBeFalsy();
-        expect(component.find('NoData').exists()).toBeTruthy();
-    });
-
-    it('should toggle isFiltersHidden', async () => {
-        let component;
-        await act(async () => {
-            component = await mount(
-                <BrowserRouter>
-                    <Movies />
-                </BrowserRouter>
-            );
-        });
-        component.update();
-        expect(component.find("[data-testid='filter-modal']").at(0).props().hidden).toBeTruthy();
-        component.find("[data-testid='filters-toggler']").simulate('click');
-        component.update();
-        expect(component.find("[data-testid='filter-modal']").at(0).props().hidden).toBeFalsy();
-    });
+    // it(`should set background-color to ${COLORS.badRate} if voteAverage < 7 `, () => {
+    //     const component = shallow(<MovieCard {...props} voteAverage={6} />);
+    //     expect(component.find('[data-testId="StFilmRate"]')).toHaveStyleRule('background-color', `${COLORS.badRate}`);
+    // });
+    //
+    // it(`should set background-color to ${COLORS.goodRate} if voteAverage >= 7`, () => {
+    //     const component = shallow(<MovieCard {...props} voteAverage={8} />);
+    //     expect(component.find('[data-testId="StFilmRate"]')).toHaveStyleRule('background-color', `${COLORS.goodRate}`);
+    // });
 });
